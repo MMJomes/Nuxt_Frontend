4ec5f264c9415d3a0be0a52fd13011caf8620d55
unknown <HeinHtet>
2023-03-23 22:55:23 +0630

    com

--- pages/about.vue
+++ pages/about.vue
@@ -1,11 +1,29 @@
 <template>
   <div>
-    <Nuxt />
-    <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequuntur dicta alias vel quidem ipsam. Quibusdam similique aut fugit iure quidem officiis error facere. Corrupti explicabo nam ea atque eius?
-    </p>
+    <v-card-title>
+      <vdiv>
+        <p class="text-center">
+          All Image List Updated By Admin
+        </p>
+      </vdiv>
+    </v-card-title>
+    <v-row>
+      <v-col v-for="item in items" :key="item.id" cols="12" sm="6" md="4">
+        <v-card class="mb-4">
+          <v-img :src="item.image" height="200px">
+            <v-img :src="'data:image/jpeg;base64,' + item.data" style="height: 200px;" />
+            <v-expand-transition>
+              <div v-if="item.showOverlay" class="overlay" />
+            </v-expand-transition>
+          </v-img>
+          <v-card-title>{{ item.id }}</v-card-title>
+          <v-card-text>{{ item.name }}</v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
   </div>
 </template>
+
 <script>
 
 import axios from 'axios'
@@ -17,10 +35,35 @@ export default {
     }
   },
   methods: {
+    async search (req) {
+      try {
+        await axios.get('http://localhost:4000/api/mydata', {
+          params: {
+            search: this.search || '' // eslint-disable-line
+          }
+        })
+      } catch (error) {
+        console.error(error)
+      }
+    },
+
     async fetchData () {
       try {
         const response = await axios.get('http://localhost:4000/api/mydata')
-        this.items = response.data
+        if (response.status === 200) {
+          console.log(response.data)
+          this.items = response.data
+        } else {
+          console.log(response.data)
+          this.items = []
+          this.items.push({
+            id: 0,
+            name: 'No Data',
+            image: 'https://cdn.vuetifyjs.com/images/cards/house.jpg',
+            data: '',
+            showOverlay: false
+          })
+        }
       } catch (error) {
         console.error(error)
       }
--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,16 @@
 <template>
   <v-app>
     <v-container>
-      <v-app-bar app color="light-blue" white>
-        <v-toolbar-title>My App</v-toolbar-title>
-        <v-spacer />
-        <v-btn>Log In</v-btn>
-      </v-app-bar>
       <v-main>
         <v-container>
           <div>
             <v-file-input v-model="file" label="Upload Image" />
-            <button @click="uploadImage">Submit</button>
-            <img v-if="imageUrl" :src="imageUrl" />
+            <button @click="uploadImage">
+              Submit
+            </button>
+            <img v-if="imageUrl" :src="imageUrl">
           </div>
+          <v-alert v-if="showSuccess" type="success">Image uploaded successfully!</v-alert>
         </v-container>
       </v-main>
     </v-container>
@@ -20,21 +18,44 @@
 </template>
 <script>
 import axios from 'axios'
-
 export default {
   data () {
     return {
       file: null,
-      imageUrl: null
+      imageUrl: null,
+      showSuccess: false,
+      showError: false
     }
   },
   methods: {
+    gotoImageList () {
+      this.$router.push('/allimagelist')
+    },
     uploadImage () {
+      if (!this.file) {
+        alert('Please select an image')
+        return
+      } else {
+        this.error = ''
+      }
       const formData = new FormData()
       formData.append('image', this.file)
       axios.post('http://localhost:4000/upload', formData)
         .then((response) => {
           this.imageUrl = response.data.imageUrl
+          if (response.status === 200) {
+            this.snackbar = true
+            this.showSuccess = true
+            this.message = 'Your image has been uploaded successfully'
+            setTimeout(() => {
+              this.showSuccess = false
+            }, 1000)
+            setTimeout(() => {
+              this.$router.push('/about')
+            }, 1000)
+            this.file = null
+          }
+          console.log(response.data)
         })
         .catch((error) => {
           console.log(error)
